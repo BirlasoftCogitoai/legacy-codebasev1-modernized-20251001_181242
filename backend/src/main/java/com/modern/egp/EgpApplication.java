79309eb41b7e627e419f3c991bb0e6012f3f9583
acm15aug-a11y <acm15aug@gmail.com>
2025-10-01 18:12:44 +0530

    Add backend/src/main/java/com/modern/egp/EgpApplication.java

---
+++ backend/src/main/java/com/modern/egp/EgpApplication.java
@@ -0,0 +1,14 @@
+package com.modern.egp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EgpApplication.class, args);
+    }
+}
+```
+
+### Controllers
