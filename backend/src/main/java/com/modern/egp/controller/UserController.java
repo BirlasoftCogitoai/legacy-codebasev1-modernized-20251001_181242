472a04fc87d6dbfd706b4d8be42e9d22233a80db
acm15aug-a11y <acm15aug@gmail.com>
2025-10-01 18:12:46 +0530

    Add backend/src/main/java/com/modern/egp/controller/UserController.java

---
+++ backend/src/main/java/com/modern/egp/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.modern.egp.controller;
+
+import com.modern.egp.dto.UserDTO;
+import com.modern.egp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        return userService.createUser(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return userService.updateUser(id, userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
+```
+
+### Service Layer
